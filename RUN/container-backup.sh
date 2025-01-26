3a990395890dc19f9d6bb68ebc809a797dd9e69f
Stefan Beckers <develop@myprs.de>
2025-01-26 12:56:16 +0100

    added basic structure to entrypoint script

--- RUN/container-backup.sh
+++ RUN/container-backup.sh
@@ -3,7 +3,7 @@
 CDEBUG=${CDEBUG:=0}
 
 
-test "$CDEBUG" -ge 2 && set -xv
+test "$CDEBUG" -ge 10 && set -xv
 
 # To have a means to run a container in case the function implemented returns immediately:
 # Set DCDEBUG >=1 and CSLEEP_SEC to the seconds to keep the the continer up.
@@ -11,4 +11,110 @@ test -n "$CSLEEP_SEC" && { test "$CDEBUG" -ge 1 &&  sleep "$CSLEEP_SEC"; }
 
 
 
+#############################################################################
+#
+#  set defaults
+#
+
+
+set_defaults ()
+
+{
+
+    [ "$CDEBUG" -ge 2 ] && echo "DEBUG2: Entering function \"set_defaults\". "
+
+
+    [ "$CDEBUG" -ge 2 ] && echo "DEBUG2: Leaving function \"set_defaults\". "
+
+}
+
+
+#############################################################################
+#
+#  check environment
+#
+
+
+check_env ()
+
+{
+
+    [ "$CDEBUG" -ge 2 ] && echo "DEBUG2: Entering function \"check_env\". "
+
+
+    [ "$CDEBUG" -ge 2 ] && echo "DEBUG2: Leaving function \"check_env\". "
+
+}
+
+
+#############################################################################
+#
+#  run pre-scripts
+#
+
+
+run_pre ()
+
+{
+
+    [ "$CDEBUG" -ge 2 ] && echo "DEBUG2: Entering function \"run_pre\". "
+
+
+    [ "$CDEBUG" -ge 2 ] && echo "DEBUG2: Leaving function \"run_pre\". "
+
+}
+
+
+
+#############################################################################
+#
+#  run restic backup
+#
+
+
+run_restic_backup ()
+
+{
+
+    [ "$CDEBUG" -ge 2 ] && echo "DEBUG2: Entering function \"run_restic_backup\". "
+
+
+    [ "$CDEBUG" -ge 2 ] && echo "DEBUG2: Leaving function \"run_restic_backup\". "
+
+}
+
+
+
+#############################################################################
+#
+#  run post-scripts
+#
+
+
+run_post ()
+
+{
+
+    [ "$CDEBUG" -ge 2 ] && echo "DEBUG2: Entering function \"run_post\". "
+
+
+    [ "$CDEBUG" -ge 2 ] && echo "DEBUG2: Leaving function \"run_post\". "
+
+}
+
+
+#############################################################################
+#
+#  main
+#
+
+set_defaults
+
+check_env
+
+run_pre
+
+run_restic_backup
+
+run_post
 
